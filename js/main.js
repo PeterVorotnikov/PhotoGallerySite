fdb411bc2978b95ef29fb4ada7d8304c07beb603
peter <peter>
2022-11-09 20:28:11 +0300

    Add login panel and panel mode control and panel closing opening

--- js/main.js
+++ js/main.js
@@ -3,6 +3,11 @@ let registrationPlaceholders;
 let registrationCheckbox;
 let registrationSubmitButton;
 
+let loginInputs;
+let loginPlaceholders;
+let loginSubmitButton;
+
+
 window.onload = function () {
     registrationInputs = document.querySelectorAll(".registration__form__input");
     registrationPlaceholders = document.querySelectorAll(".registration__placeholder");
@@ -25,6 +30,63 @@ window.onload = function () {
     }
 
     registrationCheckbox.onclick = checkRegistrationFields;
+
+
+
+    loginInputs = document.querySelectorAll(".login__form__input");
+    loginPlaceholders = document.querySelectorAll(".login__placeholder");
+    loginSubmitButton = document.querySelector(".login__submit");
+
+    for (let i = 0; i < loginInputs.length; i++) {
+        loginInputs[i].oninput = function () {
+            if (loginInputs[i].value != "") {
+                loginPlaceholders[i].classList.add("form__placeholder__mini");
+                loginInputs[i].parentNode.parentNode.classList.add("login__not-empty");
+            }
+            else {
+                loginPlaceholders[i].classList.remove("form__placeholder__mini");
+                loginInputs[i].parentNode.parentNode.classList.remove("login__not-empty");
+            }
+
+            checkLoginFields();
+        }
+    }
+
+
+
+
+    let registrationModeButton = document.querySelector(".change-mode__button__registration");
+    let loginModeButton = document.querySelector(".change-mode__button__autorization");
+    let registrationButton = document.querySelector(".header__registration");
+    let loginButton = document.querySelector(".header__login");
+    registrationModeButton.onclick = function(){
+        showRegistrationForm();
+        registrationModeButton.classList.add("change-mode__button__active");
+        loginModeButton.classList.remove("change-mode__button__active");
+    }
+    loginModeButton.onclick = function(){
+        showLoginForm();
+        registrationModeButton.classList.remove("change-mode__button__active");
+        loginModeButton.classList.add("change-mode__button__active");
+    }
+    registrationButton.onclick = function(){
+        showRegistrationForm();
+        registrationModeButton.classList.add("change-mode__button__active");
+        loginModeButton.classList.remove("change-mode__button__active");
+    }
+    loginButton.onclick = function(){
+        showLoginForm();
+        registrationModeButton.classList.remove("change-mode__button__active");
+        loginModeButton.classList.add("change-mode__button__active");
+    }
+
+
+
+    let formBackground = document.querySelector(".form__background");
+    formBackground.onclick = function(){
+        let form = document.querySelector(".form");
+        form.style.display = "none";
+    }
 }
 
 
@@ -44,4 +106,36 @@ function checkRegistrationFields() {
     }
     registrationSubmitButton.disabled = false;
     registrationSubmitButton.classList.remove("submit__unactive");
+}
+
+
+function checkLoginFields() {
+    for(let i = 0; i < loginInputs.length; i++){
+        if(loginInputs[i].value == ""){
+            loginSubmitButton.disabled = true;
+            loginSubmitButton.classList.add("submit__unactive");
+            return;
+        }
+    }
+    loginSubmitButton.disabled = false;
+    loginSubmitButton.classList.remove("submit__unactive");
+}
+
+
+function showRegistrationForm(){
+    let form = document.querySelector(".form");
+    form.style.display = "block";
+    let registrationFields = document.querySelector(".registration__form");
+    let loginFields = document.querySelector(".login__form");
+    registrationFields.style.display = "block";
+    loginFields.style.display = "none";
+}
+
+function showLoginForm(){
+    let form = document.querySelector(".form");
+    form.style.display = "block";
+    let registrationFields = document.querySelector(".registration__form");
+    let loginFields = document.querySelector(".login__form");
+    registrationFields.style.display = "none";
+    loginFields.style.display = "block";
 }
